a06b7d93df77305132b2f22fa6dcedfc1e424f24
Pauline Wairimu <paunimo@gmail.com>
2025-08-18 14:03:37 +0300

    adding constants

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,29 @@
 import os
 
 AA_DATA_DIR = os.getenv("AA_DATA_DIR") 
+
+highlight_cells = [(2023, "April"),(2024, "January")]
+
+capital_districts = [
+    "Maputo Cidade",
+    "Quelim",
+    "Cidade de Tete",
+    "Beira",
+    "Nampula",
+    "Lichinga",
+    "Matola",
+    "Cidade de Pemba",
+    "Cidade de Xai Xai",
+    "Cidade de Inhambane",
+    "Cidade de Chimoio",
+]
+
+capitals_threshold = 300
+default_threshold = 100
+
+district_case_limit = 1500
+province_case_limit = 2100
+
+# setting both of these to zero for now
+winsorising_lower_limit = 0.1
+winsorising_upper_limit = 0
